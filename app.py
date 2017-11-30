0e9b4ef70f3edc9c0292e29719dc1bb2afe1022b
diepnn94 <amy94nguyen@gmail.com>
2017-11-29 19:45:37 -0800

    Add files via upload
    Second version of Post App

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from forms import RegistrationForm
 from forms import LoginForm
 from forms import PostForm
-from flask import session,Flask, flash,redirect, url_for, request, render_template
+from flask import session,Flask, flash,redirect, url_for, request, render_template, jsonify
 import PostAppOperation
 import mailInfo
 from datetime import timedelta
 from flask_mail import Mail, Message
 from pymongo import MongoClient
-
+import os
 app = Flask(__name__)
 mail=Mail(app)
 Mailinfo = mailInfo.mailSetup();
@@ -17,15 +17,22 @@ app.config['MAIL_USERNAME'] = Mailinfo.username
 app.config['MAIL_PASSWORD'] = Mailinfo.password
 app.config['MAIL_USE_TLS'] = Mailinfo.TLS
 app.config['MAIL_USE_SSL'] = Mailinfo.SSL
+KEY = "postapptesting2017"
 mail = Mail(app)
 
 app.secret_key= 'postapptesting2017'
 
-@app.before_request
-def make_session_permanent():
-    session.permanent = True;
-    app.permanent_session_lifetime = timedelta(minutes =30);
-    # flash("The session has expired. Please log in to access PostApp.");
+# @app.before_request
+# def make_session_permanent():
+#     session.permanent = True;
+#     app.permanent_session_lifetime = timedelta(minutes =30);
+#     # flash("The session has expired. Please log in to access PostApp.");
+
+def sendEmailConfimation(name,username, receivers):
+    msg = Message("Welcome to Post App", sender = mailInfo.mailSetup().username, recipients = [receivers]);
+    msg.body = "Hello {}, thank you for joining Post App. Your username is {} and your KEY is {}.\
+    Please keep this email as a record and use the KEY to complete your registration.\nSincerely,\nPost App".format(name, username, KEY);
+    return msg;
 
 def connect():
     connection = MongoClient("ds163294.mlab.com", 63294, tz_aware = True)
@@ -34,6 +41,15 @@ def connect():
     return handle
 db = connect();
 
+@app.route("/get_file/<filename>", methods=["POST"])
+def get_file(filename):
+    images_file = [];
+    for filename1 in os.listdir(os.getcwd()+"/static/" + filename):
+        a = url_for("static", filename=filename+"/" + filename1);
+        images_file.append(a);
+    return jsonify(result = images_file);
+
+
 @app.route("/")
 def main():
     username = "";
@@ -41,105 +57,36 @@ def main():
         username = session['name'];
     return render_template('main.html', username=username);
 
-@app.route("/logout")
-def logout():
-    if 'name' in session:
-        session.pop('name', None)
-    return redirect(url_for('main'));
+@app.route("/userRecover", methods=["POST", "GET"])
+def userRecover():
+    return render_template("userRecovery.html");
 
 @app.route("/blog", methods=["POST", "GET"])
 def blog():
-    if request.method == "GET" or request.method == "POST":
-        if 'name' in session:
-            post = PostForm();
-            username = session['name'];
-            response = PostAppOperation.postMessage(db,username, post.post.data );
-            post = PostForm(formdata=None);
-            messages = PostAppOperation.getMessage(db);
-            username = {"name": username};
-            return render_template("blog.html", username=username, form=post, messages = messages);
-        else:
-            flash("You need to log in/register to access Post App.");
-            return redirect(url_for("main"));
-
-@app.route("/emailConfirmation/<username>", methods =["POST", "GET"])
-@app.route("/emailConfirmation", defaults={'username':None})
-def emailConfirmation(username):
-    if request.method == "GET":
-        return render_template("emailConfirm.html", username = username);
-
-    elif request.method == "POST":
-        key = request.form['key'];
-        response = PostAppOperation.confirmation(db,username, key);
-        if response == "success":
-            session['name'] = username;
-            return redirect(url_for("blog"))
-        elif response == "user error":
-            flash("The username does not exist in our record. Please try again.");
-            return redirect(url_for("account", option =  "login"));
-        else:
-            flash("The key entered is invalid. Please try again.")
-            return redirect(url_for("emailConfirmation", username = username));
+    return render_template("blog.html");
 
 
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        form = LoginForm();
-        if form.validate_on_submit():
-            username = form.username.data;
-            password = form.password.data;
-            response = PostAppOperation.login(db,username, password);
-            if  response == "fail":
-                flash("The information you had entered does not match our record. Please try again.")
-                return render_template("account.html", option="login", form=form);
-            elif response == "Confirm Key":
-                return redirect(url_for('emailConfirmation', username = username));
-            elif response == "success":
-                session['name'] = username;
-                post = PostForm();
-                return redirect(url_for("blog"));
-        else:
-            return render_template("account.html", option="login", form=form);
-    else:
-        return redirect(url_for("account", option =  "login"));
+
+@app.route("/emailConfirmation", methods=["POST", "GET"])
+def emailConfirmation():
+    return render_template("emailConfirm.html");
+
+
 
 @app.route("/registration", methods=["POST", "GET"])
 def registration():
     if request.method == "POST":
-        form = RegistrationForm();
-        if form.validate_on_submit():
-            name = form.name.data;
-            email = form.email.data;
-            username = form.username.data;
-            password = form.password.data;
-            response = PostAppOperation.inserttoDB(db,name, email, username, password);
-            if response == "success":
-                confirmMessage = PostAppOperation.sendEmailConfimation(name, username, email);
-                mail.send(confirmMessage);
-                return redirect(url_for('emailConfirmation', username = username));
-            else:
-                flash(response);
-                return render_template("account.html", option="registration", form=form)
+        name = request.form["name"];
+        email = request.form["email"];
+        username= request.form["username"];
+        message = sendEmailConfimation(name, username, email);
+        mail.send(message);
+        return redirect(url_for('emailConfirmation'));
 
-        else:
-            flash("All fields are required.")
-            return render_template("account.html", option="registration", form=form)
-    else:
-        return redirect(url_for("account", option="registration"));
+@app.route("/register")
+def register():
+    return render_template("register.html");
 
-@app.route("/account/<option>", methods=["POST", "GET"])
-@app.route("/account", defaults={'option':None})
-def account(option):
-    if request.method == "POST":
-        return redirect(url_for("main"));
-    else:
-        if option == "login":
-            login= LoginForm()
-            return render_template("account.html", option = option, form = login);
-        else:
-            registration = RegistrationForm();
-            return render_template("account.html", option = option, form=registration);
 
 if __name__ == '__main__':
     app.debug = True;
